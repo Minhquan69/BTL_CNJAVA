44cf4aa253526900d402feaf5f0b817cf53a48c3
Minhquan69 <daomquan2004@gmail.com>
2024-05-05 02:57:24 +0700

    frame

---
+++ project-btl-cnjava/src/main/java/com/example/pj/Main.java
@@ -0,0 +1,14 @@
+package com.example.pj;
+
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
