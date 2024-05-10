3c9c61e15fee75e1b74752d2a269e7e0c997baf9
hai <fifahayrataolahai1762004@gmail.com>
2024-05-10 23:29:09 +0700

    b123

--- project-btl-oop-master/src/main/java/com/example/pj/Controller/ItemController.java
+++ project-btl-oop-master/src/main/java/com/example/pj/Controller/ItemController.java
@@ -91,6 +91,7 @@ public class ItemController {
                     return;
                 } else {
                 }
+                return;
             }
         }
         itemsGioHang.add(item);
